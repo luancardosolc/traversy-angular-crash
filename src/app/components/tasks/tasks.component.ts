5455220193969b053e2c034aa8d21d9527d871ff
Luan Cardoso <luanleitecardoso@gmail.com>
2021-05-29 23:35:03 -0300

    Deleting tasks and updating the list

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/services/task.service';
-import {Task} from 'src/app/Task';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-tasks',
@@ -15,6 +15,14 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.taskService.getTasks().subscribe(
       (tasks) => this.tasks = tasks
+    );
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+    .deleteTask(task)
+    .subscribe(
+      () => this.tasks = this.tasks.filter(t => t.id !== task.id)
     );
   }
 

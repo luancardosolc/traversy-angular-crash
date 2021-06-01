eb43b40effeb2cce81a650e6d17598434ccb2a76
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-01 19:15:22 -0300

    Add task component created

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -33,4 +33,10 @@ export class TasksComponent implements OnInit {
     .subscribe();
   }
 
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe(
+      (task) => this.tasks.push(task)
+    );
+  }
+
 }
